2fb180c2e370a912d5d0bd7f253aba52763bce71
kiyoshi nishihara <paranishian@gmail.com>
2018-11-14 09:23:55 +0900

    first commit

---
+++ FirebaseStorageSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseStorageSample
+//
+//  Created by nishihara on 2018/11/14.
+//  Copyright © 2018年 paranishian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FirebaseStorageSampleTests/FirebaseStorageSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  FirebaseStorageSampleTests.swift
+//  FirebaseStorageSampleTests
+//
+//  Created by nishihara on 2018/11/14.
+//  Copyright © 2018年 paranishian. All rights reserved.
+//
+
+import XCTest
+@testable import FirebaseStorageSample
+
+class FirebaseStorageSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
